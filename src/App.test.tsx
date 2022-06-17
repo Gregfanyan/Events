01c58c0e551cd00f9cc83782e2f6083e79a31245
Gregfanyan <grigorfanyan@gmail.com>
2022-06-17 22:50:08 +0200

    ✅ test icon buttons

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,31 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import App from "./App";
 import { render } from "./test-utils/test-utils";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 
-test.skip("should render title", () => {
-  render(<App />);
-  const titleElement = screen.getByText(/Public Events/i);
-  expect(titleElement).toBeInTheDocument();
+describe("should render input events", () => {
+  test("should render title", () => {
+    render(<App />);
+    const titleElement = screen.getByText(/Public Events/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  test("should render search input", () => {
+    render(<App />);
+    const input = screen.getByRole("textbox", { name: /search/i });
+    expect(input).toHaveValue("");
+    userEvent.type(input, "test");
+    expect(input).toHaveValue("test");
+  });
 });
 
-test("should render search input", () => {
-  render(<App />);
-  const input = screen.getByRole("textbox", { name: /search/i });
-  expect(input).toHaveValue("");
-  userEvent.type(input, "test");
-  expect(input).toHaveValue("test");
+describe("add items to basket", () => {
+  it("should render buttons", async () => {
+    render(<App />);
+    const iconBtn = await screen.findAllByLabelText("add to favorites", {
+      selector: "button",
+    });
+    await waitFor(() => expect(iconBtn).toBeTruthy());
+  });
 });
